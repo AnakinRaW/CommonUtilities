5abe055b2db7ea791570bd92977501ba3a2cbf99
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2021-09-23 13:10:11 +0200

    add common project

---
+++ src/CommonUtilities/src/DisposableObject.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Sklavenwalker.CommonUtilities
+{
+    /// <summary>
+    /// Base implementation for classes which shall implement the <see cref="IDisposable"/> interface.
+    /// This class provides convenience like an event, a status flag and validation method.
+    /// <br/>
+    /// Disposable resources are divided in managed resources and unmanaged resources.
+    /// Managed resources get disposed when explicitly calling <see cref="Dispose()"/> on the instance.
+    /// Unmanaged resources additionally get disposed when the instance is finalized by the GC.
+    /// </summary>
+    public abstract class DisposableObject : IDisposable
+    {
+        private EventHandler? _disposing;
+
+        /// <summary>
+        /// Event which get's raised when the instance is getting disposed.
+        /// <remarks>The event is not triggered when the object is finalized by the GC.</remarks>
+        /// </summary>
+        public event EventHandler Disposing
+        {
+            add
+            {
+                ThrowIfDisposed();
+                _disposing += value;
+            }
+            remove => _disposing -= value;
+        }
+
+        /// <summary>
+        /// Indicates whether this instance is disposed.
+        /// </summary>
+        public bool IsDisposed { get; private set; }
+        
+        /// <inheritdoc/>
+        ~DisposableObject()
+        {
+            Dispose(false);
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ObjectDisposedException"/> if this instance already is disposed.
+        /// </summary>
+        protected void ThrowIfDisposed()
+        {
+            if (IsDisposed)
+                throw new ObjectDisposedException(GetType().Name);
+        }
+
+        /// <summary>
+        /// Disposes this instance and frees managed and unmanaged resources.
+        /// Once this method is called <see cref="IsDisposed"/> is set to <see langword="true"/>
+        /// </summary>
+        /// <param name="disposing">When set to <see langword="true"/> managed resources get disposed.</param>
+        protected void Dispose(bool disposing)
+        {
+            if (IsDisposed)
+                return;
+            try
+            {
+                if (disposing)
+                {
+                    _disposing?.Invoke(this, EventArgs.Empty);
+                    _disposing = null;
+                    DisposeManagedResources();
+                }
+                DisposeNativeResources();
+            }
+            finally
+            {
+                IsDisposed = true;
+            }
+        }
+
+        /// <summary>
+        /// Disposes all managed resources.
+        /// This method gets invoked if and only if the object's <see cref="Dispose()"/> method was called.
+        /// </summary>
+        protected virtual void DisposeManagedResources()
+        {
+        }
+
+        /// <summary>
+        /// Disposes all managed resources.
+        /// This method gets invoked if the object gets disposed or finalized.
+        /// </summary>
+        protected virtual void DisposeNativeResources()
+        {
+        }
+    }
+}
---
+++ src/CommonUtilities/test/DisposableObjectTest.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+
+namespace Sklavenwalker.CommonUtilities.Test
+{
+    public class DisposableObjectTest
+    {
+        [Fact]
+        public void DisposeTest()
+        {
+            var obj = new MockDisposableObject();
+            Assert.False(obj.IsDisposed);
+            
+            var eventFlag = false;
+            obj.Disposing += (_, _) => eventFlag = true;
+            obj.Dispose();
+            Assert.True(eventFlag);
+            Assert.True(obj.IsDisposed);
+            Assert.Throws<ObjectDisposedException>(() => obj.Disposing += (_, _) => {});
+            
+        }
+
+        private class MockDisposableObject : DisposableObject
+        {
+        }
+    }
+}
