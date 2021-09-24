4131697b9f46095787530f7dd7d8a7785af796b5
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2021-09-24 14:55:47 +0200

    path utilities

---
+++ src/CommonUtilities.FileSystem/src/PathNormalizeOptions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sklavenwalker.CommonUtilities.FileSystem
+{
+    /// <summary>
+    /// Options which can be performed by an path normalization.
+    /// </summary>
+    [Flags]
+    public enum PathNormalizeOptions
+    {
+        /// <summary>
+        /// Directory Separator Characters get unified across the path based on the system's preferred separator.
+        /// <remarks>On Windows this is '\'. On Linux this is '/'</remarks>
+        /// </summary>
+        UnifySlashes = 1,
+        /// <summary>
+        /// Removes any trailing directory separators at the end of a path.
+        /// </summary>
+        TrimTrailingSeparator = 2,
+        /// <summary>
+        /// On case insensitive systems (like Windows) the normalization will lower all characters.
+        /// </summary>
+        ToLowerCase = 4,
+        /// <summary>
+        /// Makes a path absolute and resolves any '..' and '.' path identifiers.
+        /// </summary>
+        ResolveFullPath = 8,
+        /// <summary>
+        /// Applies all available options.
+        /// </summary>
+        Full = UnifySlashes | TrimTrailingSeparator | ToLowerCase | ResolveFullPath
+    }
+}
---
+++ src/CommonUtilities.FileSystem/test/PathHelperServiceTest.cs
@@ -0,0 +1,72 @@
+﻿using Sklavenwalker.CommonUtilities.FileSystem;
+using Xunit;
+
+namespace Commonutilities.FileSystem.Test
+{
+    public class PathHelperServiceTest
+    {
+        private readonly PathHelperService _service;
+
+        public PathHelperServiceTest()
+        {
+            _service = new PathHelperService();
+        }
+
+        [Theory]
+        [InlineData("C:\\a", "C:\\a\\b", "a\\b")]
+        [InlineData("C:\\a\\", "D:\\a\\b", "D:\\a\\b")]
+        [InlineData("C:\\a\\", "C:\\a\\b\\", "b\\")]
+        [InlineData("C:\\a\\", "C:\\a\\b", "b")]
+        [InlineData("C:\\a\\", "C:\\A\\B", "B")]
+        [InlineData("C:\\a\\", "C:\\C\\B", "..\\C\\B")]
+        [InlineData("a", "a\\b", "a\\b")]
+        public void TestGetRelative(string basePath, string part, string expected)
+        {
+            Assert.Equal(expected, _service.GetRelativePath(basePath, part));
+        }
+
+        [Theory]
+        [InlineData("C:\\a", "C:\\a\\b", true)]
+        [InlineData("C:\\a", "C:\\a", true)]
+        [InlineData("C:\\a", "D:\\a", false)]
+        [InlineData("C:\\a\\", "C:\\a\\b\\", true)]
+        [InlineData("C:\\a\\", "C:\\a\\b", true)]
+        [InlineData("C:\\a\\", "C:\\A\\B", true)]
+        [InlineData("C:\\a\\", "C:\\C\\B", false)]
+        [InlineData("a", "a\\b", true)]
+        public void TestIsChild(string basePath, string candidate, bool expected)
+        {
+            Assert.Equal(expected, _service.IsChildOf(basePath, candidate));
+        }
+
+        [Theory]
+        [InlineData("C:\\a", "C:\\a\\")]
+        [InlineData("C:\\a\\", "C:\\a\\")]
+        public void TestEnsureTrailing(string path, string expected)
+        {
+            Assert.Equal(expected, _service.EnsureTrailingSeparator(path));
+        }
+
+        [Theory]
+        [InlineData("C:\\a", true)]
+        [InlineData("C:\\a\\", true)]
+        [InlineData("\\\\a\\", true)]
+        [InlineData("..\\a\\", false)]
+        [InlineData("a", false)]
+        public void TestIsAbsolute(string path, bool expected)
+        {
+            Assert.Equal(expected, _service.IsAbsolute(path));
+        }
+
+        [Theory]
+        [InlineData("C:\\a\\../A\\", PathNormalizeOptions.Full, "c:\\a")]
+        [InlineData("C:\\a\\../A\\", PathNormalizeOptions.TrimTrailingSeparator, "C:\\a\\../A")]
+        [InlineData("C:\\a\\../A\\", PathNormalizeOptions.ResolveFullPath, "C:\\A\\")]
+        [InlineData("C:\\a\\../A\\", PathNormalizeOptions.ToLowerCase, "c:\\a\\../a\\")]
+        [InlineData("C:\\a\\../A\\", PathNormalizeOptions.UnifySlashes, "C:\\a\\..\\A\\")]
+        public void NormalizeTest(string path, PathNormalizeOptions options, string expected)
+        {
+            Assert.Equal(expected, _service.NormalizePath(path, options));
+        }
+    }
+}
