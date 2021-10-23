a0002bb8a90cd3f66c549301099623be28bd88bf
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2021-10-23 12:34:19 +0200

    add comments

--- src/CommonUtilities.Registry/src/IRegistry.cs
+++ src/CommonUtilities.Registry/src/IRegistry.cs
@@ -1,7 +1,16 @@
-﻿namespace Sklavenwalker.CommonUtilities.Registry
+﻿namespace Sklavenwalker.CommonUtilities.Registry;
+
+/// <summary>
+/// Provides <see cref="IRegistryKey"/> objects that represent the root keys in the based on the Windows registry layout.
+/// </summary>
+public interface IRegistry
 {
-    public interface IRegistry
-    {
-        IRegistryKey OpenBaseKey(RegistryHive hive, RegistryView view);
-    }
+    /// <summary>
+    /// Opens a new <see cref="IRegistryKey"/> that represents the requested key on the local machine
+    /// with the specified view.
+    /// </summary>
+    /// <param name="hive">The HKEY to open.</param>
+    /// <param name="view">The registry view to use.</param>
+    /// <returns></returns>
+    IRegistryKey OpenBaseKey(RegistryHive hive, RegistryView view);
 }
