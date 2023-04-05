4588b403ff7bb3e42d987ed563d1791a89098a5c
AnakinRaW <anakin@gmx-topmail.de>
2023-04-05 14:00:15 +0200

    add test

--- src/CommonUtilities.SimplePipeline/src/StepFailureException.cs
+++ src/CommonUtilities.SimplePipeline/src/StepFailureException.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
 using System.Text;
 
 namespace AnakinRaW.CommonUtilities.SimplePipeline;
@@ -10,8 +9,8 @@ namespace AnakinRaW.CommonUtilities.SimplePipeline;
 /// </summary>
 public class StepFailureException : Exception
 {
-    private readonly string? _error;
-    private readonly IEnumerable<IStep>? _failedSteps;
+    private readonly string? _error = null;
+    private readonly IEnumerable<IStep> _failedSteps;
 
     /// <inheritdoc/>
     public override string Message => Error;
@@ -23,11 +22,9 @@ public class StepFailureException : Exception
             if (_error != null)
                 return _error;
             var stringBuilder = new StringBuilder();
-            if (_failedSteps != null)
-            {
-                foreach (var step in _failedSteps)
-                    stringBuilder.Append("Step '" + step + $"' failed with error: {step.Error?.Message};");
-            }
+            
+            foreach (var step in _failedSteps)
+                stringBuilder.Append("Step '" + step + $"' failed with error: {step.Error?.Message};");
             return stringBuilder.ToString().TrimEnd(';');
         }
     }
@@ -36,21 +33,8 @@ public class StepFailureException : Exception
     /// Initializes a new instance of the <see cref="StepFailureException"/> with steps that failed
     /// </summary>
     /// <param name="failedSteps"></param>
-    public StepFailureException(IEnumerable<IStep>? failedSteps)
+    public StepFailureException(IEnumerable<IStep> failedSteps)
     {
-        _failedSteps = failedSteps;
-    }
-
-    /// <inheritdoc/>
-    protected StepFailureException(SerializationInfo info, StreamingContext context) : base(info, context)
-    {
-        _error = info.GetString(nameof(Error));
-    }
-
-    /// <inheritdoc/>
-    public override void GetObjectData(SerializationInfo info, StreamingContext context)
-    {
-        base.GetObjectData(info, context);
-        info.AddValue("Error", Error);
+        _failedSteps = failedSteps ?? throw new ArgumentNullException(nameof(failedSteps));
     }
 }
