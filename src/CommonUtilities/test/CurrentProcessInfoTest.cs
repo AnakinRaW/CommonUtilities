f5f5aa1f49be57b8b77bfcafce0231511b14ad22
AnakinSklavenwalker <anakin@gmx-topmail.de>
2023-05-09 20:55:05 +0200

    remove test

--- src/CommonUtilities/test/CurrentProcessInfoTest.cs
+++ src/CommonUtilities/test/CurrentProcessInfoTest.cs
@@ -5,13 +5,6 @@ namespace AnakinRaW.CommonUtilities.Test;
 public class CurrentProcessInfoTests
 {
     [Fact]
-    public void TestDefault()
-    {
-        Assert.Equal(0, default(CurrentProcessInfo).Id);
-        Assert.Null(default(CurrentProcessInfo).ProcessFilePath);
-    }
-
-    [Fact]
     public void TestCurrentProcess()
     {
         var currentProcessInfo = CurrentProcessInfo.Current;
