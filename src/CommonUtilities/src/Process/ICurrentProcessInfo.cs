7f990a40b3f7675c833ecbf9af1720b5bc919ca2
AnakinRaW <Anakin@gmx-topmail.de>
2023-05-10 13:38:34 +0200

    Release 8 (#120)
    * Update Build (#87)
    
    * adjust build props
    
    * rename namespace
    
    * remove versions form projects
    
    * enable logging
    
    * update deps 1
    
    * use new test dl
    
    * update tests
    
    * change test
    
    * update deps 2
    
    * update deps 3
    
    * removed a couple of warnings
    
    * Versioning (#88)
    
    * add gitversion
    
    * nbgv and ci updates
    
    * add global json
    
    * test
    
    * add delay
    
    * update projects (#91)
    
    * update projects
    
    * remove url combine
    
    * update projects
    
    * Download manager update (#93)
    
    * remove net48 target
    
    * make dl fully async
    
    * start generic verification context
    
    * new verification
    
    * fix local files get disposed
    
    * disposing
    
    * make download config immutable
    
    * release 4.0
    
    * fix not using io.abstractions
    
    * add test cases
    
    * Bump Microsoft.NET.Test.Sdk from 17.4.1 to 17.5.0 (#100)
    
    Bumps [Microsoft.NET.Test.Sdk](https://github.com/microsoft/vstest) from 17.4.1 to 17.5.0.
    - [Release notes](https://github.com/microsoft/vstest/releases)
    - [Changelog](https://github.com/microsoft/vstest/blob/main/docs/releases.md)
    - [Commits](https://github.com/microsoft/vstest/compare/v17.4.1...v17.5.0)
    
    ---
    updated-dependencies:
    - dependency-name: Microsoft.NET.Test.Sdk
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump System.IO.Abstractions from 19.1.13 to 19.2.1 (#101)
    
    Bumps [System.IO.Abstractions](https://github.com/TestableIO/System.IO.Abstractions) from 19.1.13 to 19.2.1.
    - [Release notes](https://github.com/TestableIO/System.IO.Abstractions/releases)
    - [Commits](https://github.com/TestableIO/System.IO.Abstractions/compare/v19.1.13...v19.2.1)
    
    ---
    updated-dependencies:
    - dependency-name: System.IO.Abstractions
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * update deps
    
    * New pipeline (#105)
    
    * rename and move
    
    * move ns
    
    * renames
    
    * add progress
    
    * rename
    
    * update progress
    
    * added tests
    
    * update ref
    
    * rename and move
    
    * move ns
    
    * renames
    
    * add progress
    
    * rename
    
    * update progress
    
    * added tests
    
    * update ref
    
    * use .net 7 sdk
    
    * use preview
    
    * import parent
    
    * add test
    
    * update version
    
    * add process elevation
    
    * ignore test
    
    * use config provider
    
    * fix build from merge
    
    * update and move verification
    
    * update deps
    
    * to class
    
    * update deps
    
    * add CurrentProcessInfo
    
    * increase version
    
    * merge cpi and elevation
    
    * remove test
    
    * move namespace
    
    * don't move ns
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ src/CommonUtilities/src/Process/ICurrentProcessInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace AnakinRaW.CommonUtilities;
+
+/// <summary>
+/// Service to query whether the current process is elevated.
+/// </summary>
+public interface ICurrentProcessInfo
+{
+    /// <summary>
+    /// Returns <see langword="true"/> if the current process is elevated; <see langword="false"/> otherwise.
+    /// </summary>
+    bool IsElevated { get; }
+
+    /// <summary>
+    /// Gets the file path of the current process, or null if the path could not be determined.
+    /// </summary>
+    string? ProcessFilePath { get; }
+
+    /// <summary>
+    /// Gets the ID of the current process.
+    /// </summary>
+    int Id { get; }
+}
---
+++ src/CommonUtilities/test/CurrentProcessInfoTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace AnakinRaW.CommonUtilities.Test;
+
+public class CurrentProcessInfoTests
+{
+    [Fact]
+    public void TestCurrentProcess()
+    {
+        var currentProcessInfo = CurrentProcessInfo.Current;
+        Assert.NotEqual(0, currentProcessInfo.Id);
+        Assert.NotNull(currentProcessInfo.ProcessFilePath);
+    }
+}
