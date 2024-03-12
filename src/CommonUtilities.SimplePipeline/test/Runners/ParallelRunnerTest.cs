7ab7d7d214e7e86e243c9d4e31295c681e7159d0
AnakinRaW <anakin@gmx-topmail.de>
2024-03-12 11:02:30 +0100

    add missing test from after rebase

---
+++ src/CommonUtilities.SimplePipeline/test/Runners/ParallelRunnerTest.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AnakinRaW.CommonUtilities.SimplePipeline.Runners;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace AnakinRaW.CommonUtilities.SimplePipeline.Test.Runners;
+
+public class ParallelRunnerTest
+{
+    [Fact]
+    public void Test_Wait()
+    {
+        var sc = new ServiceCollection();
+        var runner = new ParallelRunner(2, sc.BuildServiceProvider());
+
+        var s1 = new Mock<IStep>();
+        var s2 = new Mock<IStep>();
+
+        runner.Queue(s1.Object);
+        runner.Queue(s2.Object);
+
+        var ran1 = false;
+        s1.Setup(t => t.Run(default)).Callback(() =>
+        {
+            Task.Delay(1000);
+            ran1 = true;
+        });
+        var ran2 = false;
+        s2.Setup(t => t.Run(default)).Callback(() =>
+        {
+            Task.Delay(1000);
+            ran2 = true;
+        });
+
+
+        runner.Run(default);
+        runner.Wait();
+
+        Assert.True(ran1);
+        Assert.True(ran2);
+    }
+
+    [Fact]
+    public void Test_Run_NoWait()
+    {
+        var sc = new ServiceCollection();
+        var runner = new ParallelRunner(2, sc.BuildServiceProvider());
+
+        var s1 = new Mock<IStep>();
+        var s2 = new Mock<IStep>();
+
+        var b = new ManualResetEvent(false);
+
+        runner.Queue(s1.Object);
+        runner.Queue(s2.Object);
+
+        var ran1 = false;
+        s1.Setup(t => t.Run(default)).Callback(() =>
+        {
+            b.WaitOne();
+            ran1 = true;
+        });
+        var ran2 = false;
+        s2.Setup(t => t.Run(default)).Callback(() =>
+        {
+            b.WaitOne();
+            ran2 = true;
+        });
+
+
+        runner.Run(default);
+        Assert.False(ran1);
+        Assert.False(ran2);
+        b.Set();
+    }
+
+    [Fact]
+    public void Test_Wait_Timeout_ThrowsTimeoutException()
+    {
+        var sc = new ServiceCollection();
+        var runner = new ParallelRunner(2, sc.BuildServiceProvider());
+
+        var s1 = new Mock<IStep>();
+
+        var b = new ManualResetEvent(false);
+
+        runner.Queue(s1.Object);
+
+        s1.Setup(t => t.Run(default)).Callback(() =>
+        {
+            b.WaitOne();
+        });
+
+        runner.Run(default);
+        Assert.Throws<TimeoutException>(() => runner.Wait(TimeSpan.FromMilliseconds(100)));
+        b.Set();
+    }
+
+    [Fact]
+    public void Test_Run_WithError()
+    {
+        var sc = new ServiceCollection();
+        var runner = new ParallelRunner(2, sc.BuildServiceProvider());
+
+        var hasError = false;
+        runner.Error += (_, _) =>
+        {
+            hasError = true;
+        };
+
+        var step = new Mock<IStep>();
+        var ran = false;
+        step.Setup(t => t.Run(default)).Callback(() =>
+        {
+            ran = true;
+        }).Throws<Exception>();
+
+        runner.Queue(step.Object);
+        runner.Run(default);
+        runner.Wait(Timeout.InfiniteTimeSpan);
+
+        Assert.True(hasError);
+        Assert.True(ran);
+        Assert.NotNull(runner.Exception);
+    }
+
+    [Fact]
+    public void Test_Run_Cancelled()
+    {
+        var sc = new ServiceCollection();
+        var runner = new ParallelRunner(1, sc.BuildServiceProvider());
+
+        var cts = new CancellationTokenSource();
+
+        var b = new ManualResetEvent(false);
+
+        var hasError = false;
+        runner.Error += (_, _) =>
+        {
+            hasError = true;
+        };
+
+        var t1 = new Mock<IStep>();
+        var ran = false;
+        t1.Setup(t => t.Run(cts.Token)).Callback(() =>
+        {
+            ran = true;
+            cts.Cancel();
+            b.Set();
+        });
+        var t2 = new Mock<IStep>();
+        t2.Setup(t => t.Run(cts.Token)).Callback(() =>
+        {
+            b.WaitOne();
+        });
+
+        runner.Queue(t1.Object);
+        runner.Queue(t2.Object);
+        runner.Run(cts.Token);
+        runner.Wait(Timeout.InfiniteTimeSpan);
+
+        Assert.True(hasError);
+        Assert.True(ran);
+        Assert.True(runner.IsCancelled);
+    }
+}
---
+++ src/CommonUtilities.SimplePipeline/test/Steps/PipelineStepTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+using AnakinRaW.CommonUtilities.SimplePipeline.Steps;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Moq.Protected;
+using Xunit;
+
+namespace AnakinRaW.CommonUtilities.SimplePipeline.Test.Steps;
+
+public class PipelineStepTest
+{
+    [Fact]
+    public void Test_Disposed()
+    {
+        var sc = new ServiceCollection();
+        var step = new Mock<PipelineStep>(sc.BuildServiceProvider())
+        {
+            CallBase = true
+        };
+
+        step.Object.Dispose();
+        Assert.True(step.Object.IsDisposed);
+    }
+
+    [Fact]
+    public void Test_Run()
+    {
+        var sc = new ServiceCollection();
+        var step = new Mock<PipelineStep>(sc.BuildServiceProvider())
+        {
+            CallBase = true
+        };
+
+        step.Object.Run(default);
+
+        step.Protected().Verify("RunCore", Times.Exactly(1), false, (CancellationToken)default);
+    }
+
+    [Fact]
+    public void Test_Run_ThrowsException()
+    {
+        var sc = new ServiceCollection();
+        var step = new Mock<PipelineStep>(sc.BuildServiceProvider())
+        {
+            CallBase = true
+        };
+
+        var e = new Exception();
+        step.Protected().Setup("RunCore", false, (CancellationToken)default)
+            .Throws(e);
+
+        Assert.Throws<Exception>(() => step.Object.Run(default));
+        Assert.Same(e, step.Object.Error);
+
+    }
+
+    [Fact]
+    public void Test_Run_WithCancellation_ThrowsOperationCanceledException()
+    {
+        var sc = new ServiceCollection();
+        var step = new Mock<PipelineStep>(sc.BuildServiceProvider())
+        {
+            CallBase = true
+        };
+
+        var cts = new CancellationTokenSource();
+        cts.Cancel();
+
+        step.Protected().Setup("RunCore", false, cts.Token)
+            .Callback<CancellationToken>(d => d.ThrowIfCancellationRequested());
+
+        Assert.Throws<OperationCanceledException>(() => step.Object.Run(cts.Token));
+        Assert.Null(step.Object.Error);
+    }
+}
