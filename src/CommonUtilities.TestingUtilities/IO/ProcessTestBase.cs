bb9b9697230396e465eba5873c49c05b55e378fa
AnakinRaW <anakin@gmx-topmail.de>
2025-01-01 19:56:14 +0100

    start new tests for Process

---
+++ src/CommonUtilities.TestingUtilities/IO/ProcessTestBase.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Microsoft.DotNet.RemoteExecutor;
+
+namespace AnakinRaW.CommonUtilities.Testing.IO;
+
+public class ProcessTestBase
+{
+    public const int WaitInMS = 5 * 60 * 1000;
+
+    private readonly List<Process> _processes = new();
+
+    protected Process CreateProcessLong([CallerMemberName] string callerName = null)
+    {
+        return CreateSleepProcess(WaitInMS, callerName);
+    }
+
+    protected Process CreateSleepProcess(int durationMs, [CallerMemberName] string callerName = null)
+    {
+        return CreateProcess(Sleep, durationMs.ToString(), callerName);
+    }
+
+    public static int Sleep(string duration, string callerName)
+    {
+        _ = callerName; // argument ignored, for debugging purposes
+        Thread.Sleep(int.Parse(duration));
+        return 42;
+    }
+
+    private Process CreateProcess(Func<string, string, int> method, string arg1, string arg2, bool autoDispose = true)
+    {
+        Process p;
+        using (var handle = RemoteExecutor.Invoke(method, arg1, arg2, new RemoteInvokeOptions { Start = false }))
+        {
+            p = handle.Process;
+            handle.Process = null;
+        }
+
+        if (autoDispose)
+        {
+            AddProcessForDispose(p);
+        }
+
+        return p;
+    }
+
+    private void AddProcessForDispose(Process p)
+    {
+        lock (_processes)
+        {
+            _processes.Add(p);
+        }
+    }
+
+    public void Dispose()
+    {
+        // Wait for all started processes to complete
+        foreach (Process p in _processes)
+        {
+            try
+            {
+                p.Kill();
+                Assert.True(p.WaitForExit(WaitInMS));
+                p.WaitForExit(); // wait for event handlers to complete
+            }
+            catch (InvalidOperationException) { } // in case it was never started
+        }
+    }
+}
--- src/CommonUtilities/test/AwaitExtensionsTests.cs
+++ src/CommonUtilities/test/AwaitExtensionsTests.cs
@@ -1,105 +1,163 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using AnakinRaW.CommonUtilities.Testing;
+using AnakinRaW.CommonUtilities.Testing.IO;
 using Xunit;
 
 namespace AnakinRaW.CommonUtilities.Test;
 
 // From https://github.com/microsoft/vs-threading
-public class AwaitExtensionsTests
+public class AwaitExtensionsTests : ProcessTestBase
 {
     [Fact]
-    public async Task Test_WaitForExitAsync_NullArgument()
+    public async Task MultipleProcesses_StartAllKillAllWaitAllAsync()
     {
-        await Assert.ThrowsAsync<ArgumentNullException>(() => AwaitExtensions.WaitForExitAsync(null!));
-    }
+        const int Iters = 10;
+        var processes = Enumerable.Range(0, Iters).Select(_ => CreateProcessLong()).ToArray();
 
-    [PlatformSpecificFact(TestPlatformIdentifier.Windows)]
-    public async Task Test_WaitForExitAsync_ExitCode_Windows()
-    {
-        var p = Process.Start(
-            new ProcessStartInfo("cmd.exe", "/c exit /b 55")
-            {
-                CreateNoWindow = true,
-                WindowStyle = ProcessWindowStyle.Hidden,
-            })!;
-        await AwaitExtensions.WaitForExitAsync(p);
-        Assert.Equal(55, p.ExitCode);
-    }
-
-    [PlatformSpecificFact(TestPlatformIdentifier.Windows)]
-    public void Test_WaitForExitAsync_AlreadyExited_Windows()
-    {
-        var p = Process.Start(
-            new ProcessStartInfo("cmd.exe", "/c exit /b 55")
-            {
-                CreateNoWindow = true,
-                WindowStyle = ProcessWindowStyle.Hidden,
-            })!;
-        p.WaitForExit();
-        var t = AwaitExtensions.WaitForExitAsync(p);
-        Assert.True(t.IsCompleted);
-        Assert.Equal(55, p.ExitCode);
-    }
-
-    [Fact]
-    public async Task Test_WaitForExitAsync_UnstartedProcess()
-    {
-        var processName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "cmd.exe" : "/bin/bash";
-        var process = new Process();
-        process.StartInfo.FileName = processName;
-        process.StartInfo.CreateNoWindow = true;
-        await Assert.ThrowsAsync<InvalidOperationException>(() => process.WaitForExitAsync());
-    }
-
-    [Fact]
-    public async Task Test_WaitForExitAsync_DoesNotCompleteTillKilled()
-    {
-        var processName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "cmd.exe" : "/bin/bash";
-        var expectedExitCode = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? -1 : 128 + 9; // https://stackoverflow.com/a/1041309
-        var p = Process.Start(new ProcessStartInfo(processName) { CreateNoWindow = true, WindowStyle = ProcessWindowStyle.Hidden })!;
-        try
+        foreach (var p in processes) p.Start();
+        foreach (var p in processes) p.Kill();
+        foreach (var p in processes)
         {
-            var t = AwaitExtensions.WaitForExitAsync(p);
-            Assert.False(t.IsCompleted);
-            p.Kill();
-            await t;
-            Assert.Equal(expectedExitCode, p.ExitCode);
+            using (var cts = new CancellationTokenSource(WaitInMS))
+            {
+                await p.WaitForExitAsync(cts.Token);
+                Assert.True(p.HasExited);
+            }
         }
-        catch
+    }
+
+    [Fact]
+    public async Task MultipleProcesses_SerialStartKillWaitAsync()
+    {
+        const int Iters = 10;
+        for (int i = 0; i < Iters; i++)
         {
-            try
+            Process p = CreateProcessLong();
+            p.Start();
+            p.Kill();
+            using (var cts = new CancellationTokenSource(WaitInMS))
             {
+                await p.WaitForExitAsync(cts.Token);
+                Assert.True(p.HasExited);
+            }
+        }
+    }
+
+    [Fact]
+    public async Task MultipleProcesses_ParallelStartKillWaitAsync()
+    {
+        const int Tasks = 4, ItersPerTask = 10;
+        Func<Task> work = async () =>
+        {
+            for (int i = 0; i < ItersPerTask; i++)
+            {
+                Process p = CreateProcessLong();
+                p.Start();
                 p.Kill();
+                using (var cts = new CancellationTokenSource(WaitInMS))
+                {
+                    await p.WaitForExitAsync(cts.Token);
+                    Assert.True(p.HasExited);
+                }
             }
-            catch
-            {
-                // Ignore
-            }
+        };
 
-            throw;
-        }
+        await Task.WhenAll(Enumerable.Range(0, Tasks).Select(_ => Task.Run(work)));
     }
 
-    [Fact]
-    public async Task Test_WaitForExitAsync_Canceled()
-    {
-        var processName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "cmd.exe" : "/bin/bash";
-        var p = Process.Start(new ProcessStartInfo(processName) { CreateNoWindow = true, WindowStyle = ProcessWindowStyle.Hidden })!;
-        try
-        {
-            var cts = new CancellationTokenSource();
-            var t = AwaitExtensions.WaitForExitAsync(p, cts.Token);
-            Assert.False(t.IsCompleted);
-            cts.Cancel();
-            await Assert.ThrowsAsync<TaskCanceledException>(() => t);
-        }
-        finally
-        {
-            p.Kill();
-        }
-    }
+
+    //[Fact]
+    //public async Task Test_WaitForExitAsync_NullArgument()
+    //{
+    //    await Assert.ThrowsAsync<ArgumentNullException>(() => AwaitExtensions.WaitForExitAsync(null!));
+    //}
+
+    //[PlatformSpecificFact(TestPlatformIdentifier.Windows)]
+    //public async Task Test_WaitForExitAsync_ExitCode_Windows()
+    //{
+    //    var p = Process.Start(
+    //        new ProcessStartInfo("cmd.exe", "/c exit /b 55")
+    //        {
+    //            CreateNoWindow = true,
+    //            WindowStyle = ProcessWindowStyle.Hidden,
+    //        })!;
+    //    await AwaitExtensions.WaitForExitAsync(p);
+    //    Assert.Equal(55, p.ExitCode);
+    //}
+
+    //[PlatformSpecificFact(TestPlatformIdentifier.Windows)]
+    //public void Test_WaitForExitAsync_AlreadyExited_Windows()
+    //{
+    //    var p = Process.Start(
+    //        new ProcessStartInfo("cmd.exe", "/c exit /b 55")
+    //        {
+    //            CreateNoWindow = true,
+    //            WindowStyle = ProcessWindowStyle.Hidden,
+    //        })!;
+    //    p.WaitForExit();
+    //    var t = AwaitExtensions.WaitForExitAsync(p);
+    //    Assert.True(t.IsCompleted);
+    //    Assert.Equal(55, p.ExitCode);
+    //}
+
+    //[Fact]
+    //public async Task Test_WaitForExitAsync_UnstartedProcess()
+    //{
+    //    var processName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "cmd.exe" : "/bin/bash";
+    //    var process = new Process();
+    //    process.StartInfo.FileName = processName;
+    //    process.StartInfo.CreateNoWindow = true;
+    //    await Assert.ThrowsAsync<InvalidOperationException>(() => process.WaitForExitAsync());
+    //}
+
+    //[Fact]
+    //public async Task Test_WaitForExitAsync_DoesNotCompleteTillKilled()
+    //{
+    //    var processName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "cmd.exe" : "/bin/bash";
+    //    var expectedExitCode = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? -1 : 128 + 9; // https://stackoverflow.com/a/1041309
+    //    var p = Process.Start(new ProcessStartInfo(processName) { CreateNoWindow = true, WindowStyle = ProcessWindowStyle.Hidden })!;
+    //    try
+    //    {
+    //        var t = AwaitExtensions.WaitForExitAsync(p);
+    //        Assert.False(t.IsCompleted);
+    //        p.Kill();
+    //        await t;
+    //        Assert.Equal(expectedExitCode, p.ExitCode);
+    //    }
+    //    catch
+    //    {
+    //        try
+    //        {
+    //            p.Kill();
+    //        }
+    //        catch
+    //        {
+    //            // Ignore
+    //        }
+
+    //        throw;
+    //    }
+    //}
+
+    //[Fact]
+    //public async Task Test_WaitForExitAsync_Canceled()
+    //{
+    //    var processName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "cmd.exe" : "/bin/bash";
+    //    var p = Process.Start(new ProcessStartInfo(processName) { CreateNoWindow = true, WindowStyle = ProcessWindowStyle.Hidden })!;
+    //    try
+    //    {
+    //        var cts = new CancellationTokenSource();
+    //        var t = AwaitExtensions.WaitForExitAsync(p, cts.Token);
+    //        Assert.False(t.IsCompleted);
+    //        cts.Cancel();
+    //        await Assert.ThrowsAsync<TaskCanceledException>(() => t);
+    //    }
+    //    finally
+    //    {
+    //        p.Kill();
+    //    }
+    //}
 }
