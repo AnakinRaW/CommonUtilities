13cae3959210b44021d8c3de8578ad5b1f2bc883
AnakinRaW <anakin@gmx-topmail.de>
2026-01-01 18:21:53 +0100

    change target

--- src/CommonUtilities.SimplePipeline/src/Steps/RunPipelineStep.cs
+++ src/CommonUtilities.SimplePipeline/src/Steps/RunPipelineStep.cs
@@ -1,42 +1,42 @@
-﻿//using System;
-//using System.Linq;
-//using System.Threading;
-//using Microsoft.Extensions.Logging;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
-//namespace AnakinRaW.CommonUtilities.SimplePipeline.Steps;
+namespace AnakinRaW.CommonUtilities.SimplePipeline.Steps;
 
-///// <summary>
-///// A step that executes a pipeline and waits for the pipeline to end.
-///// </summary>
-///// <param name="pipeline">The pipeline to execute.</param>
-///// <param name="serviceProvider">The service provider</param>
-//public class RunPipelineStep(IPipeline pipeline, IServiceProvider serviceProvider) : SynchronizedStep(serviceProvider)
-//{
-//    private readonly IPipeline _pipeline = pipeline ?? throw new ArgumentNullException(nameof(pipeline));
+/// <summary>
+/// A step that executes a pipeline and waits for the pipeline to end.
+/// </summary>
+/// <param name="pipeline">The pipeline to execute.</param>
+/// <param name="serviceProvider">The service provider</param>
+public sealed class RunPipelineStep(IPipeline pipeline, IServiceProvider serviceProvider) : PipelineStep(serviceProvider)
+{
+    private readonly IPipeline _pipeline = pipeline ?? throw new ArgumentNullException(nameof(pipeline));
 
-//    /// <inheritdoc />
-//    protected override void RunSynchronized(CancellationToken token)
-//    {
-//        Logger?.LogTrace("Running {Pipeline}...", _pipeline);
-//        try
-//        {
-//            // ReSharper disable once MethodSupportsCancellation
-//            _pipeline.RunAsync(token).Wait();
-//            Logger?.LogTrace("Finished {Pipeline}", _pipeline);
-//        }
-//        catch (AggregateException e)
-//        {
-//            var root = e.InnerExceptions.FirstOrDefault();
-//            if (root is not null)
-//                throw root;
-//            throw;
-//        }
-//    }
+    /// <inheritdoc />
+    protected override async Task RunCoreAsync(CancellationToken token)
+    {
+        Logger?.LogTrace("Running {Pipeline}...", _pipeline);
+        try
+        {
+            await _pipeline.RunAsync(token).ConfigureAwait(false);
+            Logger?.LogTrace("Finished {Pipeline}", _pipeline);
+        }
+        catch (AggregateException e)
+        {
+            var root = e.InnerExceptions.FirstOrDefault();
+            if (root is not null)
+                throw root;
+            throw;
+        }
+    }
 
-//    /// <inheritdoc />
-//    protected override void DisposeResources()
-//    {
-//        base.DisposeResources();
-//        _pipeline.Dispose();
-//    }
-//}
+    /// <inheritdoc />
+    protected override void DisposeResources()
+    {
+        base.DisposeResources();
+        _pipeline.Dispose();
+    }
+}
